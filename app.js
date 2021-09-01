c533e2a7972fcae51b8ea115610912885650eb46
nico-gao <jtgao99@gmail.com>
2021-08-31 17:03:48 -0700

    Add ejs templates, bootstrap, change campground schema

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -20,6 +21,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -50,6 +52,7 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
 
 app.post('/campgrounds', async (req, res) => {
     const camp = new Campground(req.body.camp);
+    console.log(camp);
     await camp.save();
     res.redirect(`/campgrounds/${camp._id}`)
 });
@@ -64,7 +67,11 @@ app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const camp = await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-})
+});
+
+app.use((req, res) => {
+    res.status(404).send("NOT FOUND");
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
