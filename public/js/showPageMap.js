8b848b18121b517086780138d4c9d8b8e43edf34
nico-gao <jtgao99@gmail.com>
2021-09-21 18:36:26 -0700

    Add map to campground show page

---
+++ public/js/showPageMap.js
@@ -0,0 +1,18 @@
+mapboxgl.accessToken = mapToken;
+console.log(camp.geometry);
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: camp.geometry.coordinates, // starting position [lng, lat]
+zoom: 10 // starting zoom
+});
+
+const marker = new mapboxgl.Marker()
+.setLngLat(camp.geometry.coordinates)
+.setPopup(
+    new mapboxgl.Popup({ offset: 25 })
+    .setHTML(
+        `<h5>${camp.title}</h5><p>${camp.location}</p>`
+    )
+)
+.addTo(map);
